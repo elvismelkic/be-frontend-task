b14347b30c74998f8312ac52d2147062a3079451
Elvis <elvis.melkic@gmail.com>
2019-03-25 19:11:32 +0100

    implemented changing colors on clicking the component

--- src/components/BeComponent.js
+++ src/components/BeComponent.js
@@ -6,7 +6,8 @@ export default class BeComponent extends Component {
   state = {
     loading: true,
     error: null,
-    colors: []
+    colors: [],
+    activeColor: "#000"
   };
 
   getColors = async () => {
@@ -16,7 +17,7 @@ export default class BeComponent extends Component {
       const color2 = await fetchColors();
 
       this.setState({
-        colors: [color1.new_color, color2.new_color],
+        colors: [`#${color1.new_color}`, `#${color2.new_color}`],
         loading: false
       });
     } catch (error) {
@@ -27,19 +28,38 @@ export default class BeComponent extends Component {
     }
   };
 
+  handleClick = () => {
+    if (this.state.activeColor !== "#000") {
+      this.setState({ activeColor: "#000" });
+    } else {
+      let randomColor =
+        Math.random() > 0.5 ? this.state.colors[0] : this.state.colors[1];
+
+      this.setState({ activeColor: randomColor });
+
+      console.log(randomColor);
+    }
+  };
+
   componentDidMount() {
     this.getColors();
   }
 
   render() {
-    const { loading, error, colors } = this.state;
+    const { loading, error, activeColor } = this.state;
     console.log(this.state);
     return (
       <Fragment>
         {error !== null ? (
           <h1>{error}</h1>
         ) : loading === false ? (
-          <div className="be-component">{this.props.text}</div>
+          <div
+            className="be-component"
+            style={{ backgroundColor: activeColor }}
+            onClick={this.handleClick}
+          >
+            {this.props.text}
+          </div>
         ) : (
           <h1>LOADING</h1>
         )}
